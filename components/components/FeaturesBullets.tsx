e63ce61675cc681814a6755d45b92d27eb307e65
Rakshit <rakshit087@gmail.com>
2025-09-23 23:49:02 +0530

    feat: added features bullets

---
+++ components/components/FeaturesBullets.tsx
@@ -0,0 +1,35 @@
+export default function FeaturesBullets() {
+  return (
+    <div className="flex justify-between gap-8 w-full items-stretch lg:flex-col lg:w-1/3 lg:gap-12">
+      <div className="flex-1 flex flex-col">
+        <div className="flex-grow">
+          <h3 className="text-2xl font-bold text-white mb-4 font-lato">Connect</h3>
+          <p className="text-white/80 font-lato">
+            Point your app to Viper's RPC endpoint.
+          </p>
+        </div>
+        <div className="w-full h-1 bg-gradient-to-r to-[#473782] from-[#BCAFEB] mt-4" />
+      </div>
+
+      <div className="flex-1 flex flex-col">
+        <div className="flex-grow">
+          <h3 className="text-2xl font-bold text-white mb-4 font-lato">Query</h3>
+          <p className="text-white/80 font-lato">
+            We route your requests to the best-performing nodes.
+          </p>
+        </div>
+        <div className="w-full h-1 bg-gradient-to-r to-[#473782] from-[#BCAFEB] mt-4" />
+      </div>
+
+      <div className="flex-1 flex flex-col">
+        <div className="flex-grow">
+          <h3 className="text-2xl font-bold text-white mb-4 font-lato">Scale</h3>
+          <p className="text-white/80 font-lato">
+            Get guaranteed uptime & fast response at 3x cheaper.
+          </p>
+        </div>
+        <div className="w-full h-1 bg-gradient-to-r from-[#473782] to-[#BCAFEB] mt-4" />
+      </div>
+    </div>
+  )
+}
