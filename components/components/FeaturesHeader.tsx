dd720da00d247bf372ee82137cf84930bfb85952
Rakshit <rakshit087@gmail.com>
2025-09-23 23:48:44 +0530

    feat: added features header

---
+++ components/components/FeaturesHeader.tsx
@@ -0,0 +1,12 @@
+export default function FeaturesHeader() {
+  return (<div className="text-center flex flex-col gap-8 py-24">
+    <h2 className="text-4xl md:text-5xl font-bold text-white font-inter">
+      Built for Developers,<br />
+      Built for Web3
+    </h2>
+    <p className="text-xl text-white/70 max-w-3xl mx-auto font-space-grotesk">
+      Powered by a global fleet of nodes—delivering security, reliability, and
+      scalability for every blockchain app.
+    </p>
+  </div>)
+}
