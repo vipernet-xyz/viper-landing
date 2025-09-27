9c8fa8f7e3a7c1883a6afa6d459b6c68fc47aee6
Rakshit <rakshit087@gmail.com>
2025-09-28 00:29:48 +0530

    style: made navbar look better on mobile and fixed the hover effects

--- components/components/Navbar.tsx
+++ components/components/Navbar.tsx
@@ -3,8 +3,8 @@ import { Button } from "../ui/button";
 
 export default function Navbar() {
   return (
-    <nav className="relative z-10 mx-auto w-full max-w-7xl px-4 py-8">
-      <div className="glass-effect px-8 py-4">
+    <nav className="z-20 w-full max-w-7xl px-2 py-4 lg:px-4 lg:py-8 fixed left-1/2 -translate-x-1/2">
+      <div className="glass-effect px-4 lg:px-8 py-2 lg:py-4">
         <div className="flex items-center justify-between">
           <Image
             src="assets/logo.svg"
@@ -34,7 +34,9 @@ export default function Navbar() {
             </a>
           </div>
 
-          <Button className="bg-white text-black hover:bg-white/90 font-space-grotesk font-medium">
+          <Button
+            className="bg-white text-black hover:bg-[#9c7ff162] hover:text-white hover:border border-white border-[1px] font-space-grotesk font-medium text-xs lg:text-base"
+          >
             Get Started
           </Button>
         </div>
