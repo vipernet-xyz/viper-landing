e36cac98bd75740a8b036a0a18a554b6ba2026d1
Rakshit <rakshit087@gmail.com>
2025-11-06 21:16:47 +0530

    refactor: replace plain text with links in FooterLinks component

--- components/components/FooterLinks.tsx
+++ components/components/FooterLinks.tsx
@@ -1,22 +1,55 @@
+import Link from "next/link";
+
 export default function FooterLinks() {
   return (
     <div className="flex gap-12 py-12 lg:py-24 px-4">
       <div className="font-bold text-white font-space-grotesk">
         <h3 className="text-xl mb-2">+ Resources:</h3>
         <div className="space-y-2">
-          <p>Blogs</p>
-          <p>Doc</p>
+          <div>
+          <Link href={"https://medium.com/@vipernet"} target="_blank">
+            Blogs
+          </Link>
+          </div>
+          <div>
+          <Link href={"https://docs.vipernet.xyz/"} target="_blank">
+            Doc
+          </Link>
+          </div>
         </div>
       </div>
 
       <div className="font-bold text-white font-space-grotesk">
         <h3 className="text-xl mb-2">+ Community:</h3>
         <div className="space-y-2">
-          <p>X [Twitter]</p>
-          <p>Github</p>
-          <p>Discord</p>
-          <p>Medium</p>
-          <p>Mail</p>
+          <div>
+            <Link href={"https://x.com/viper_network_"} target="_blank">
+              X [Twitter]
+            </Link>
+          </div>
+          <div>
+            <Link href={"https://github.com/vipernet-xyz"} target="_blank">
+              Github
+            </Link>
+          </div>
+          <div>
+            <Link
+              href={"https://discord.com/invite/eBDYH4Zxek"}
+              target="_blank"
+            >
+              Discord
+            </Link>
+          </div>
+          <div>
+            <Link href={"https://medium.com/@vipernet"} target="_blank">
+              Medium
+            </Link>
+          </div>
+          <div>
+            <Link href={"mailto:contact@vipernet.xyz"} target="_blank">
+              Mail
+            </Link>
+          </div>
         </div>
       </div>
     </div>
